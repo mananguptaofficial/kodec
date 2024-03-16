02f526e32a25cb41fc4310a517c9b8c07f14cfd9
mananguptaofficial <111212931+mananguptaofficial@users.noreply.github.com>
2024-03-16 21:32:38 +0530

    Update codec.py
    Error

--- codec.py
+++ codec.py
@@ -20,6 +20,8 @@ def fetch_balance(address):
             return balance_element[0].text_content().strip()
     return "No data available"
 
+import time
+
 def search_addresses(core_number, winner_file, sert_shared):
     addresses = set(pd.read_csv("btc.txt", header=None).iloc[:, 0])
     w = 0
@@ -32,12 +34,17 @@ def search_addresses(core_number, winner_file, sert_shared):
         with sert_shared.get_lock():
             sert_shared.value += 1
             sert = sert_shared.value
-            with open(winner_file, "a") as f:
-                if balance != '0 BTC' or address in addresses:
-                    w += 1
-                    f.write(f'\nADDR: {address}     BAL: {balance}\nKEY: {private_key}\n------------------------(MMDRZA.COM)---------------------\n')
-                else:
-                    console.print(f'[gold1]{sert}[/][green] : {address} : [/][yellow]{private_key}[/][green] :  - B:[/][gold1]{balance}[/][b green]  W:[/][with]{w}[/]')
+            if balance != "No data available":
+                with open(winner_file, "a") as f:
+                    if balance != '0 BTC' or address in addresses:
+                        w += 1
+                        f.write(f'\nADDR: {address}     BAL: {balance}\nKEY: {private_key}\n------------------------(MMDRZA.COM)---------------------\n')
+                    else:
+                        console.print(f'[gold1]{sert}[/][green] : {address} : [/][yellow]{private_key}[/][green] :  - B:[/][gold1]{balance}[/][b green]  W:[/][with]{w}[/]')
+            else:
+                console.print(f'[red]No data available for address:[/][gold1]{address}[/]')
+                time.sleep(15)  # Wait for 15 seconds before retrying
+
 
 if __name__ == '__main__':
     num_cores = 12  # Use 12 cores
